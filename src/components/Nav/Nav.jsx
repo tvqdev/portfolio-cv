d036508472a1a19fca28026d5789aaed25638220
Quang <tvq.dev@gmail.com>
2022-07-13 16:48:58 +0700

    fix menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,11 +13,11 @@ const Nav = () => {
 
      return (
           <nav key={1}>
-               <Link to="/" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}> <AiOutlineHome /></Link>
-               <Link to="about" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></Link>
-               <Link to="experience" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></Link>
-               <Link to="portfolio" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiMessageSquare /></Link>
-               <Link to="contact" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiServiceLine /></Link>
+               <Link to="/" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('/')}> <AiOutlineHome /></Link>
+               <Link to="about" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#about')} ><AiOutlineUser /></Link>
+               <Link to="experience" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#experience')} ><BiBook /></Link>
+               <Link to="portfolio" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#portfolio')} ><BiMessageSquare /></Link>
+               <Link to="contact" spy={true} smooth={true} offset={50} duration={500} onClick={() => setActiveNav('#contact')} ><RiServiceLine /></Link>
 
           </nav>
      );
